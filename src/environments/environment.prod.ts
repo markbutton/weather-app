121fb5f636cd4502dacb99186f0fe2694cce3126
Mark Button <mark.button@gmail.com>
2018-11-03 14:41:47 -0600

    fomat DESIGN.md and add https call for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   appId: 'f9ae7ad578a6d06724a140168b4dfc95',
-  baseUrl: 'http://api.openweathermap.org/data/2.5/',
+  baseUrl: 'https://api.openweathermap.org/data/2.5/',
   units: 'imperial'
 };
